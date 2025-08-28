8815341af07f761222f85e9fccf14fdfc97a2a18
Mr.Skyler <rishavsinha9199@gmail.com>
2025-08-28 10:33:59 +0530

    just started with basics in student marks management

---
+++ high-end-projects/Python/Student _Marks _Manager.py
@@ -0,0 +1,13 @@
+#practicing list and stuff
+
+#student marks manager -mini project 
+
+#taking help of google 
+
+marks = list(input("Enter students marks : ").split(","))
+print("Students marks list : ",marks)
+
+#using append to append the list 
+marks.append(27)
+print("Students marks Updated list : ",marks)
+
